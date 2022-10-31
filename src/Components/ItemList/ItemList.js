3a7a333def4c07b858c91105c292ad56a072a182
Ben Sam <ben.ss@inapp.com>
2022-10-31 15:28:51 +0530

    added carting from home page

--- src/Components/ItemList/ItemList.js
+++ src/Components/ItemList/ItemList.js
@@ -9,30 +9,37 @@ function ItemList() {
     const [itemsInCart, setItemsIncart] = useState(0)
 
     const getItems = async () => {
-      console.log('inside get items')
       await axios.get("http://localhost:7000/items")
         .then(res => {
-          console.log(res)
           setItems(res.data)
         })
     }
 
-    console.log('inside itemlist')
     useEffect(() => {
-      console.log('inside useeffect')
       getItems();
     }, [])
 
-    const addToCart = () => {
+    const addToCart = async (item) => {
       setItemsIncart(itemsInCart + 1);
-      console.log(itemsInCart);
+      // console.log(itemsInCart);
+      // console.log(item.id);
+      let num= item.numOfCarted+1
+      console.log(num)
+      await axios.put("http://localhost:7000/items/"+item.id, {
+        ...item,
+        "cart":true,
+        "numOfCarted":item.numOfCarted + 1
+      }).then( res => {
+        console.log(res);
+        getItems();
+        })
     }
     
 
   return (
     <div className='item-container'>
         {items.map((item) => (
-          <Card className='itemCard' title={item.name} header={<img className='cardImg' alt="Card" src={item.image}/>} footer={<Button onClick={ addToCart } label="Add to Cart"></Button>}>
+          <Card key={items.id} className='itemCard' title={item.name} header={<img className='cardImg' alt="Card" src={item.image}/>} footer={<Button onClick={ ()=>addToCart(item) } label="Add to Cart"></Button>}>
           </Card>
         ))
         }
