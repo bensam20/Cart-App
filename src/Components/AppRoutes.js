b126f830ee1ce1830a307d689a03bca14c5004b7
Ben Sam <ben.ss@inapp.com>
2022-11-16 16:58:47 +0530

    Lazyloader to home ,cart routes. Localstorage done

--- src/Components/AppRoutes.js
+++ src/Components/AppRoutes.js
@@ -1,19 +1,36 @@
 import React from 'react'
 import { Routes, Route, Navigate } from "react-router-dom";
 import PrivateRoutes from '../routes/PrivateRoutes';
-import Cart from './CartPage/Cart';
-import Home from './Home/Home';
+// import Cart from './CartPage/Cart';
+// import Home from './Home/Home';
 import Login from './Login/Login';
+const LazyHome = React.lazy(() => import('./Home/Home') );
+const LazyCart = React.lazy(() => import('./CartPage/Cart'))
 
 function AppRoutes() {
   return (
     <Routes>
-        <Route path="/" element={<Home />} />
+        <Route 
+          path="/" 
+          element={
+            <React.Suspense fallback='Loading...'>
+              <LazyHome />
+            </React.Suspense>} />
         <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<Home />} />
+        <Route 
+          path="/home" 
+          element={
+            <React.Suspense fallback='Loading...'>
+              <LazyHome />
+            </React.Suspense>} />
         <Route path="*" element={<Navigate to={'/login'}/>} />
         <Route element={<PrivateRoutes/>}>
-          <Route path="/cart" element={<Cart />} />
+          <Route 
+            path="/cart" 
+            element={
+              <React.Suspense fallback='Loading...'>
+                <LazyCart />
+              </React.Suspense>} />
         </Route>
     </Routes>
   )
